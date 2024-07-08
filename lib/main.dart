192ea447e7ead591b8cae99b1a5c2a618c2bd8b9
suryatripatih <suryatripatih@gmail.com>
2024-07-08 09:17:36 +0000

    belajar flutter (container & stack)

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+
+import 'package:flutter/material.dart';
+import 'package:myapp/container_widget.dart';
+import 'package:myapp/news_container.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        appBar: AppBar(
+          title: Text('Belajar Flutter'),
+          centerTitle: true,
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: NewsContainer(),
+      ),
+    );
+  }
+}
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Hello Dunia',
+        style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.black26),
+      ),
+    );
+  }
+}
